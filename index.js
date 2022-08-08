f6c26a34fa5372e798055892fccb7f0f6f6de1a7
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-08-08 11:42:45 +0700

    import mongoose

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const mongoose = require("mongoose");
+
 const express = require('express')
 const app = express()
 const port = 3000
+
 
 app.get('/', (req, res) => {
   res.send('Hello kunminttt!')
