77a324fce3d2d2a002f2313417ea701d10d5b74a
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-08-08 11:55:50 +0700

    add comment models

--- models/models.js
+++ models/models.js
@@ -1,1 +1,6 @@
 //Models จะเก็บส่วนที่เป็น struct(structure) คือส่วนที่เราจะกำหนด
+
+//1. ประกาศ schema ในนี้ (type struct)
+
+//2. สร้าง model (class)
+// example const student = mongoose.model('Student', studentschma) แล้วใส่ func run (async,awiat, savedata, .then)
