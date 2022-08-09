e20bda0369d6861122c2ed9d5f982bd43b95479f
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-08-09 22:23:23 +0700

    draf userModels

---
+++ routers/userModels.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  user_id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    required: true,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+    select: false,
+  },
+  age: {
+    type: Number,
+    default: 0,
+  },
+  weight: {
+    type: Number,
+    default: 0,
+  },
+  height: {
+    type: Number,
+    default: 0,
+  },
+  bmi: {
+    type: Number,
+    default: 0,
+  },
+  user_photo: {
+    type: String,
+    required: true,
+  }
+});
+
+const userModels = new mongoose.model("User", userSchema);
+
+module.exports = userModels;
