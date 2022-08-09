27f43bac3e8d88a38b6fa503f8ca5b51d5a00b79
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-08-09 21:18:53 +0700

    draft index

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
-const mongoose = require('mongoose');
-const express = require('express')
-const router = require('./routers/routers')
+const express = require("express");
+const app = express();
 
-const app = express()
-const port = 3000
+const bodyParser = require("body-parser");
 
-app.use('/main', router);
-// app.use('/', router);
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+require("dotenv").config();
 
+const mongoose = require("mongoose");
 
-app.listen(port, () => {
-  console.log(`Server app listening on port ${port}`)
-})
+app.use(async (req, res, next) => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(500).send();
+  }
+});
+
+app.get("/", (req, res, next) => {
+  res.send("test");
+});
+
+// /activities
+// const activityRoutes = require("./routes/activitiesRoute");
+// app.use("/activities", activityRoutes);
+
+// // /users
+// const userRoutes = require("./routes/userRoute");
+// app.use("/users", userRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Express server listening on port " + PORT);
+});
