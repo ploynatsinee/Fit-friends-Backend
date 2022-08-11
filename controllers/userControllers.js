f057f295e385753ac710f8ed7b435584f4a5ef20
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-08-11 13:19:08 +0700

    test

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -15,7 +15,7 @@ const getUserById = async (req, res, next) => {
   const user = await User.findOne({ user_id });
 
   if (!user) {
-    return res.status(404).send({ message: "User not found" });
+    return res.status(404).send({ message: "User not found!" });
   }
 
   res.send(user);
